a4c0ce6e2efeb33e79813e9c3b5a480cd67f198d
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2025-02-27 17:10:45 +0500

    перела верстку ячейки на фрем

--- Test/Screens/Reviews/Cells/ReviewCountCell.swift
+++ Test/Screens/Reviews/Cells/ReviewCountCell.swift
@@ -9,41 +9,54 @@ import UIKit
 // MARK: - ReviewCountCellConfig
 struct ReviewCountCellConfig: TableCellConfig {
 	static let reuseId = String(describing: ReviewCountCellConfig.self)
-	
+
 	/// Общее количество отзывов.
 	let reviewCount: Int
-	
+
 	func update(cell: UITableViewCell) {
 		guard let cell = cell as? ReviewCountCell else { return }
 		cell.countLabel.text = "\(reviewCount) отзывов"
 	}
-	
+
 	func height(with size: CGSize) -> CGFloat {
-		return 44 // Фиксированная высота для простоты
+		return 44
 	}
 }
 
 final class ReviewCountCell: UITableViewCell {
 	let countLabel = UILabel()
-	
+
 	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
 		super.init(style: style, reuseIdentifier: reuseIdentifier)
 		setupUI()
 	}
-	
+
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
 
-	private func setupUI() {
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		layoutFrames()
+	}
+}
+
+// MARK: - Layout
+private extension ReviewCountCell {
+	func setupUI() {
 		countLabel.textAlignment = .center
 		countLabel.font = .reviewCount
 		countLabel.textColor = .reviewCount
 		contentView.addSubview(countLabel)
-		countLabel.translatesAutoresizingMaskIntoConstraints = false
-		NSLayoutConstraint.activate([
-			countLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-			countLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
-		])
+	}
+
+	func layoutFrames() {
+		let labelSize = countLabel.intrinsicContentSize
+		countLabel.frame = CGRect(
+			x: (contentView.bounds.width - labelSize.width) / 2,
+			y: (contentView.bounds.height - labelSize.height) / 2,
+			width: labelSize.width,
+			height: labelSize.height
+		)
 	}
 }

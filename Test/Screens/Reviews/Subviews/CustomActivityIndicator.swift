416edccc44af5a0ab944346f2d8ba682a29febfd
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2025-02-27 17:44:48 +0500

    minor changes

--- Test/Screens/Reviews/Subviews/CustomActivityIndicator.swift
+++ Test/Screens/Reviews/Subviews/CustomActivityIndicator.swift
@@ -13,16 +13,16 @@ final class CustomActivityIndicatorView: UIView {
 	private let dotCount = 8
 	private let animationDuration: CFTimeInterval = 1.2
 	private let radiusMultiplier: CGFloat = 1.5
-	
+
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 		setupAnimation()
 	}
-	
+
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
-	
+
 	private func setupAnimation() {
 		let radius = dotSize * radiusMultiplier
 		let dot = CALayer()
@@ -31,14 +31,14 @@ final class CustomActivityIndicatorView: UIView {
 		dot.cornerRadius = dotSize / 2
 		dot.backgroundColor = UIColor.systemBlue.cgColor
 		dot.opacity = 0.8
-		
+
 		dotLayer.addSublayer(dot)
 		dotLayer.instanceCount = dotCount
 		dotLayer.instanceTransform = CATransform3DMakeRotation((2 * .pi) / CGFloat(dotCount), 0, 0, 1)
-		
+
 		layer.addSublayer(dotLayer)
 	}
-	
+
 	private func createAnimation() -> CABasicAnimation {
 		let animation = CABasicAnimation(keyPath: "opacity")
 		animation.fromValue = 1
@@ -48,20 +48,20 @@ final class CustomActivityIndicatorView: UIView {
 		animation.autoreverses = true
 		return animation
 	}
-	
+
 	override func layoutSubviews() {
 		super.layoutSubviews()
 		dotLayer.bounds = bounds
 		dotLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
 	}
-	
+
 	func startAnimating() {
 		isHidden = false
 		if dotLayer.sublayers?.first?.animation(forKey: "fade") == nil {
 			dotLayer.sublayers?.first?.add(createAnimation(), forKey: "fade")
 		}
 	}
-	
+
 	func stopAnimating() {
 		isHidden = true
 		dotLayer.sublayers?.first?.removeAnimation(forKey: "fade")
